2ed935316e000b4cc1c4dd65dfec9a1b321bb770
y6367 <yjoe2035@gmail.com>
2023-09-12 12:37:51 -0700

    Reset button works.

--- script.js
+++ script.js
@@ -1,22 +1,43 @@
 const container = document.querySelector('.container');
+const reset = document.querySelector('.reset');
+let boxCount = 20;
+let temp = `${32 / boxCount}vw`;
+let box;
 
+function updateTemp() {
+    temp = `${32 / boxCount}vw`;
+}
 
-document.addEventListener("DOMContentLoaded", () => {
-    const box = document.querySelectorAll('.box')
+function boxInit() {
+    const boxes = document.createElement('div');
+    boxes.classList.add('box');
+    boxes.style.height = `${temp}`
+    boxes.style.width = `${temp}`
+    container.append(boxes);
+}
+
+function boxCreate() {
+    for (i = 0; i < boxCount * boxCount; i++) {
+        boxInit()
+    }
+    box = document.querySelectorAll('.box')
     box.forEach(box => {
         box.addEventListener("mouseover", () => {
             box.classList.add('active')
             console.log('lol')
         })
     });
-})
-
-function boxCreate() {
-    const boxes = document.createElement('div');
-    boxes.classList.add('box')
-    container.append(boxes);
 }
 
-for (i = 0; i < 256; i++) {
+boxCreate();
+
+
+reset.addEventListener("click", () => {
+    boxCount = prompt("Please enter the number of pixels per line up to 100.")
+    updateTemp();
+    console.log(boxCount)
+    box.forEach((div) => {
+        div.parentNode.removeChild(div);
+    });
     boxCreate()
-}
+})
